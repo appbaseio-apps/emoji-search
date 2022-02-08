116f27b15f5a2967b95fed2c338d07504a18ce3e
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-02-08 09:32:44 +0530

    feat: add step 3 sentiment analysis section

---
+++ steps/step_3_adding_the_sentiment_analysis_section/src/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ResultsRenderer from "./components/ResultRenderer";
+
+import SentimentStats from "./components/SentimentStats";
+import "./index.css";
+// import the appbase-js lib
+import appbasejs from "appbase-js";
+// instantiating appbase-js required
+// fields to connect to the dataset
+var appbaseRef = appbasejs({
+  url: "https://appbase-demo-ansible-abxiydt-arc.searchbase.io",
+  app: "emoji-dataset",
+  credentials: "f1da7b624918:3331c67d-3477-4b24-aa89-aefc6ca4683e",
+});
+
+const App = () => {
+  const [searchText, setSearchText] = useState("");
+  // indicates whether api has responded or not
+  const [isSearching, setIsSearching] = useState(false);
+  // keeping record of the fetched results
+  const [results, setResults] = useState(null);
+  const [sentimentData, setSentimentData] = useState([]);
+
+  const makeApiCall = () => {
+    setIsSearching(true);
+    const SEARCH_ID = "emoji_search";
+    appbaseRef
+      .reactiveSearch(
+        [
+          {
+            id: SEARCH_ID,
+            size: 10,
+            ...(searchText && { value: searchText }),
+          },
+        ],
+        {
+          enableQueryRules: true,
+        }
+      )
+      .then(function (res) {
+        setIsSearching(false);
+        setResults(res[SEARCH_ID].hits.hits);
+        setSentimentData(res.analysis);
+      })
+      .catch(function (err) {
+        console.log("search error: ", err);
+      });
+  };
+
+  return (
+    <div className="app-root">
+      <div className="input-wrapper">
+        <input
+          name="search-field"
+          type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
+          placeholder="Try searching, 'man swimming' "
+        />
+        <button id="recommend-btn" onClick={makeApiCall}>
+          Recommend
+        </button>
+      </div>
+      <SentimentStats sentimentData={sentimentData} />
+      <div className="result-wrapper">
+        <ResultsRenderer results={results} />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
